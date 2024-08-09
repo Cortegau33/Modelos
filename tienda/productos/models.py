141c91f473882fc7ddbeb30be4b24a114a46e260
Cortegau33 <couchin27@gmail.com>
2024-08-08 20:47:50 -0500

    app de prueba
    en este commit se sube todo el codigo

---
+++ tienda/productos/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class Cliente (models,Model):
+    nombre = models.CharField(max_length=100)
+    email = models.EmailField()
+    telefono = models.CharField(max_length=15)
+
+    def _str_(self):
+        return str(self.nombre)
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+
+    def _str_(self):
+        return str(self.nombre)
