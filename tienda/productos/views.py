2b669c34c9c1704ec26c35a88e22b75af64d91ae
Cortegau33 <couchin27@gmail.com>
2024-08-15 17:13:37 -0500

    Cambios 3ra clase

--- tienda/productos/views.py
+++ tienda/productos/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from.models import Cliente
 from.models import Producto
+from.forms import ProductoModelForm, ClienteModelForm
 # Create your views here.
 
 def listar_clientes(request):
@@ -13,3 +14,23 @@ def listar_productos(request):
     clientes = Producto.objects.all()
     return render (request, 'listar_productos.html',
     {'productos' : productos})
+
+def agregar_producto (request):
+    if request.method == 'POST' :
+        form = ProductoModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect ('listar_productos')
+        else:
+            form= ProductoModelForm
+        return render(request, 'agregar_producto.html', {'form' : form})
+    
+def agregar_cliente(request):
+    if request.method == 'POST' :
+        form = ClienteModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect ('listar_productos')
+        else:
+            form= ClienteModelForm
+        return render(request, 'agregar_producto.html', {'form' : form})
